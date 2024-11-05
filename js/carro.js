f79c69edf26a2cdd734fb706ebbbfa87d3ed5165
Juan Grisales <dimangous@gmail.com>
2024-11-05 00:39:15 -0500

    termino segundo entrega con leves cambios css

---
+++ js/carro.js
@@ -0,0 +1,85 @@
+let carrito = localStorage.getItem("productosCarrito")
+let contenedorCarrito = document.getElementById("contenedor-videojuegos")
+
+carrito = JSON.parse(carrito)
+
+function traerCarrito(videojuegosCarrito) {
+    contenedorCarrito.innerHTML = ""
+
+    if (videojuegosCarrito.length === 0) {
+        const mensajeVacio = document.createElement("p")
+        mensajeVacio.textContent = "El carro de compras está vacío."
+        contenedorCarrito.appendChild(mensajeVacio)
+    } else {
+        let totalPrecio = 0
+        videojuegosCarrito.forEach(videojuego => {
+            const tarjeta = document.createElement("div-tarjeta")
+            tarjeta.classList.add("tarjeta")
+            tarjeta.innerHTML =
+                `<h4>${videojuego.nombre}</h4>
+                            <p>${videojuego.precio}</p>
+                            <button class = "eliminarProducto" id=${videojuego.id}>-</button>`
+
+            contenedorCarrito.appendChild(tarjeta)
+            totalPrecio += parseFloat(videojuego.precio)
+        })
+
+        const totalPrecioElemento = document.createElement("p")
+        totalPrecioElemento.classList.add("total-precio")
+        totalPrecioElemento.textContent = `Total: $${totalPrecio.toFixed(2)}`
+        contenedorCarrito.appendChild(totalPrecioElemento)
+
+        const botonContinuarCompra = document.createElement("button")
+        botonContinuarCompra.textContent = "Continuar con la compra"
+        botonContinuarCompra.classList.add("continuarCompra")
+
+        botonContinuarCompra.onclick = (e) => {
+            const alertaGracias = document.createElement("p")
+            alertaGracias.textContent = "Gracias por tu compra!"
+            contenedorCarrito.appendChild(alertaGracias)
+            e.currentTarget.disabled = true
+            }
+
+        const contenedorBotonVaciar = document.createElement("div")
+        contenedorBotonVaciar.classList.add("contenedor-boton-vaciar")
+        const botonVaciar = document.createElement("button")
+
+        botonVaciar.textContent = "Vaciar carro de compras"
+        botonVaciar.classList.add("vaciarProducto")
+
+        botonVaciar.onclick = () => {
+            videojuegosCarrito.length = 0
+            localStorage.setItem("productosCarrito", JSON.stringify(videojuegosCarrito))
+
+            traerCarrito(videojuegosCarrito)
+        }
+
+        contenedorCarrito.appendChild(botonContinuarCompra)
+
+        contenedorCarrito.appendChild(botonVaciar)
+        
+
+        agregarBotonEliminar()
+    }
+}
+
+traerCarrito(carrito)
+
+function eliminarProductoDelCarrito(idProducto) {
+    carrito = carrito.filter(producto => producto.id != idProducto)
+    console.log("Producto eliminado:", carrito)
+
+    localStorage.setItem("productosCarrito", JSON.stringify(carrito))
+
+    traerCarrito(carrito)
+}
+
+function agregarBotonEliminar() {
+    const eliminarBoton = document.querySelectorAll(".eliminarProducto")
+    eliminarBoton.forEach(boton => {
+        boton.onclick = (e) => {
+            const productoId = e.currentTarget.id
+            eliminarProductoDelCarrito(productoId)
+        }
+    })
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+/*SECCION PRIMER ENTREGA CON PROMPT
 function bienvenida() {
     alert("¡Bienvenido a Vicious Library, tienda virtual de videojuegos!")
     alert("A continuación te dejaremos una lista de videojuegos:")
@@ -92,61 +93,113 @@ function compra () {
     }
 }
 
-const opcion1 = {
-    nombre: "Silent Hill 2",
-    sinopsis: "«Me llamo... Maria», dice la mujer, sonriendo. Su rostro, su voz... Es igual que ella.",
-    generos: "Terror",
-    precio: 69.99 + " USD",
-}
-
-const opcion2 = {
-    nombre: "DRAGON BALL: Sparking! ZERO",
-    sinopsis: "DRAGON BALL: Sparking! ZERO lleva a un nuevo nivel el legendario estilo de juego de la serie Budokai Tenkaichi. ¡Domina el poder destructivo de los luchadores más fuertes que han aparecido en DRAGON BALL!",
-    generos: "Acción",
-    precio: 65.99 + " USD",
-}
-
-const opcion3 = {
-    nombre: "Elden Ring",
-    sinopsis: "EL NUEVO JUEGO DE ROL Y ACCIÓN DE AMBIENTACIÓN FANTÁSTICA. Álzate, Sinluz, y que la gracia te guíe para abrazar el poder del Círculo de Elden y encumbrarte como señor del Círculo en las Tierras Intermedias.",
-    generos: "Tipo Dark Souls",
-    precio: 59.99 + " USD",
-}
-
-const opcion4 = {
-    nombre: "Project Zomboid",
-    sinopsis: "«un RPG survival horror tipo sandbox a cargo de The Indie Stone para PC en el que debemos sobrevivir a un apocalipsis zombi, solos o en modo cooperativo. Busca recursos, construye armas y equipamiento, lucha por tu vida y mucho más en un mundo infestado de zombis.",
-    generos: "Zombies",
-    precio: 19.99 + " USD",
-}
-
-const opcion5 = {
-    nombre: "Sea of Thieves: 2024 Edition",
-    sinopsis: "Sea of Thieves es un exitoso juego de aventuras piratas que ofrece la experiencia pirata por excelencia de saquear tesoros perdidos, batallas intensas, vencer monstruos marinos y más. Sumérgete en esta edición revisada del juego, que incluye acceso a medios digitales de bonificación.",
-    generos: "Multijugador",
-    precio: 39.99 + " USD"
-}
-
-const opcion6 = {
-    nombre: "ARMORED CORE VI FIRES OF RUBICON",
-    sinopsis: "Un nuevo juego de acción inspirado en el concepto de la serie ARMORED CORE que aprovecha el conocimiento adquirido durante el desarrollo de los últimos juegos de acción de FromSoftware.",
-    generos: "Mechas",
-    precio: 45.99 + " USD",
-}
-
-const opcion7 = {
-    nombre: "Red Dead Redemption 2",
-    sinopsis: "Con más de 175 premios al Juego del año y más de 250 valoraciones perfectas, Red Dead Redemption 2 es la épica historia de Arthur Morgan y la banda de Van der Linde, que huyen por toda América en el albor de una nueva era. También incluye acceso al mundo multijugador compartido de Red Dead Online.",
-    generos: "Mundo abierto",
-    precio: 69.99 + " USD",
-}
-
-const videojuegos = [opcion1, opcion2, opcion3, opcion4, opcion5, opcion6, opcion7]
-
 bienvenida()
 opciones()
 interes()
+*/
+
+const videojuegos = [
+    {
+        id: 1,
+        nombre: "Silent Hill 2",
+        sinopsis: "«Me llamo... Maria», dice la mujer, sonriendo. Su rostro, su voz... Es igual que ella.",
+        generos: "Terror",
+        precio: 69.99,
+        img: "../assets/IgwsFz9BiBrFvyV7pIWpoVgd.jpg"
+    },
+
+    {
+        id: 2,
+        nombre: "DRAGON BALL: Sparking! ZERO",
+        sinopsis: "DRAGON BALL: Sparking! ZERO lleva a un nuevo nivel el legendario estilo de juego de la serie Budokai Tenkaichi. ¡Domina el poder destructivo de los luchadores más fuertes que han aparecido en DRAGON BALL!",
+        generos: "Acción",
+        precio: 65.99,
+        img: "../assets/cbb03393f0ab1149f2b89a8194ce19e596a24fa5bec6526a.jpg",
+    },
+
+    {
+        id: 3,
+        nombre: "Elden Ring",
+        sinopsis: "EL NUEVO JUEGO DE ROL Y ACCIÓN DE AMBIENTACIÓN FANTÁSTICA. Álzate, Sinluz, y que la gracia te guíe para abrazar el poder del Círculo de Elden y encumbrarte como señor del Círculo en las Tierras Intermedias.",
+        generos: "Tipo Dark Souls",
+        precio: 59.99,
+        img: "../assets/phvVT0qZfcRms5qDAk0SI3CM.jpg",
+    },
+
+    {
+        id: 4,
+        nombre: "Project Zomboid",
+        sinopsis: "«un RPG survival horror tipo sandbox a cargo de The Indie Stone para PC en el que debemos sobrevivir a un apocalipsis zombi, solos o en modo cooperativo. Busca recursos, construye armas y equipamiento, lucha por tu vida y mucho más en un mundo infestado de zombis.",
+        generos: "Zombies",
+        precio: 19.99,
+        img: "../assets/Boxshot_of_video_game_Project_zomboid.jpg",
+    },
+
+    {
+        id: 5,
+        nombre: "Sea of Thieves: 2024 Edition",
+        sinopsis: "Sea of Thieves es un exitoso juego de aventuras piratas que ofrece la experiencia pirata por excelencia de saquear tesoros perdidos, batallas intensas, vencer monstruos marinos y más. Sumérgete en esta edición revisada del juego, que incluye acceso a medios digitales de bonificación.",
+        generos: "Multijugador",
+        precio: 39.99,
+        img: "../assets/9p2n57mc619k.jpg",
+    },
+
+    {
+        id: 6,
+        nombre: "ARMORED CORE VI FIRES OF RUBICON",
+        sinopsis: "Un nuevo juego de acción inspirado en el concepto de la serie ARMORED CORE que aprovecha el conocimiento adquirido durante el desarrollo de los últimos juegos de acción de FromSoftware.",
+        generos: "Mechas",
+        precio: 45.99,
+        img: "../assets/f9336cf87ddc9d5996606fd2e7f5e4e6a6bbc5131845d665.jpg",
+    },
+
+    {
+        id: 7,
+        nombre: "Red Dead Redemption 2",
+        sinopsis: "Con más de 175 premios al Juego del año y más de 250 valoraciones perfectas, Red Dead Redemption 2 es la épica historia de Arthur Morgan y la banda de Van der Linde, que huyen por toda América en el albor de una nueva era. También incluye acceso al mundo multijugador compartido de Red Dead Online.",
+        generos: "Mundo abierto",
+        precio: 69.99,
+        img: "../assets/icon0.jpg",
+    },
+]
+
+let videojuegosContenedor = document.getElementById("contenedor-videojuegos")
+let productosCarrito = []
+
+function traerVideojuegos(videojuegosCatalogo) {
+
+    videojuegosCatalogo.forEach(videojuego => {
+        const tarjeta = document.createElement("div-tarjeta")
+        tarjeta.classList.add("tarjeta")
+        tarjeta.innerHTML =
+                            `<h2>${videojuego.nombre}</h2>
+                            <img class="img-videojuego" src=${videojuego.img} alt=${videojuego.nombre}>
+                            <p>${videojuego.sinopsis}</p>
+                            <p>${videojuego.precio}</p>
+                            <button class = "agregarProducto" id=${videojuego.id}>Agregar al carro</button>`
+        videojuegosContenedor.appendChild(tarjeta)
+    })
+    agregarBotonAlCarrito()
+}
+
+traerVideojuegos(videojuegos)
 
 
+function agregarBotonAlCarrito() {
+    agregarBoton = document.querySelectorAll(".agregarProducto")
+    agregarBoton.forEach(agrega => {
+        agrega.onclick = (e) => {
+            const videojuegoId = e.currentTarget.id
 
+            const productoSeleccionado = videojuegos.find(videojuego => videojuego.id == videojuegoId)
+
+            productosCarrito.push(productoSeleccionado)
+            console.log(productosCarrito)
+
+            localStorage.setItem("productosCarrito", JSON.stringify(productosCarrito))
+
+            e.currentTarget.disabled = true
+        }
+    })
+}
 
