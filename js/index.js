a5118bed50062e424fda7536325152b5c3453d3a
Juan Grisales <dimangous@gmail.com>
2024-11-05 01:01:38 -0500

    linkeo imagenes correctamente

--- js/index.js
+++ js/index.js
@@ -105,16 +105,15 @@ const videojuegos = [
         sinopsis: "«Me llamo... Maria», dice la mujer, sonriendo. Su rostro, su voz... Es igual que ella.",
         generos: "Terror",
         precio: 69.99,
-        img: "./assets/IgwsFz9BiBrFvyV7pIWpoVgd.jpg",
+        img: "assets/IgwsFz9BiBrFvyV7pIWpoVgd.jpg",
     },
-
     {
         id: 2,
         nombre: "DRAGON BALL: Sparking! ZERO",
         sinopsis: "DRAGON BALL: Sparking! ZERO lleva a un nuevo nivel el legendario estilo de juego de la serie Budokai Tenkaichi. ¡Domina el poder destructivo de los luchadores más fuertes que han aparecido en DRAGON BALL!",
         generos: "Acción",
         precio: 65.99,
-        img: "../assets/cbb03393f0ab1149f2b89a8194ce19e596a24fa5bec6526a.jpg",
+        img: "assets/cbb03393f0ab1149f2b89a8194ce19e596a24fa5bec6526a.jpg",
     },
 
     {
@@ -123,7 +122,7 @@ const videojuegos = [
         sinopsis: "EL NUEVO JUEGO DE ROL Y ACCIÓN DE AMBIENTACIÓN FANTÁSTICA. Álzate, Sinluz, y que la gracia te guíe para abrazar el poder del Círculo de Elden y encumbrarte como señor del Círculo en las Tierras Intermedias.",
         generos: "Tipo Dark Souls",
         precio: 59.99,
-        img: "../assets/phvVT0qZfcRms5qDAk0SI3CM.jpg",
+        img: "assets/phvVT0qZfcRms5qDAk0SI3CM.jpg",
     },
 
     {
@@ -132,7 +131,7 @@ const videojuegos = [
         sinopsis: "«un RPG survival horror tipo sandbox a cargo de The Indie Stone para PC en el que debemos sobrevivir a un apocalipsis zombi, solos o en modo cooperativo. Busca recursos, construye armas y equipamiento, lucha por tu vida y mucho más en un mundo infestado de zombis.",
         generos: "Zombies",
         precio: 19.99,
-        img: "../assets/Boxshot_of_video_game_Project_zomboid.jpg",
+        img: "assets/Boxshot_of_video_game_Project_zomboid.jpg",
     },
 
     {
@@ -141,7 +140,7 @@ const videojuegos = [
         sinopsis: "Sea of Thieves es un exitoso juego de aventuras piratas que ofrece la experiencia pirata por excelencia de saquear tesoros perdidos, batallas intensas, vencer monstruos marinos y más. Sumérgete en esta edición revisada del juego, que incluye acceso a medios digitales de bonificación.",
         generos: "Multijugador",
         precio: 39.99,
-        img: "../assets/9p2n57mc619k.jpg",
+        img: "assets/9p2n57mc619k.jpg",
     },
 
     {
@@ -150,7 +149,7 @@ const videojuegos = [
         sinopsis: "Un nuevo juego de acción inspirado en el concepto de la serie ARMORED CORE que aprovecha el conocimiento adquirido durante el desarrollo de los últimos juegos de acción de FromSoftware.",
         generos: "Mechas",
         precio: 45.99,
-        img: "../assets/f9336cf87ddc9d5996606fd2e7f5e4e6a6bbc5131845d665.jpg",
+        img: "assets/f9336cf87ddc9d5996606fd2e7f5e4e6a6bbc5131845d665.jpg",
     },
 
     {
@@ -159,7 +158,7 @@ const videojuegos = [
         sinopsis: "Con más de 175 premios al Juego del año y más de 250 valoraciones perfectas, Red Dead Redemption 2 es la épica historia de Arthur Morgan y la banda de Van der Linde, que huyen por toda América en el albor de una nueva era. También incluye acceso al mundo multijugador compartido de Red Dead Online.",
         generos: "Mundo abierto",
         precio: 69.99,
-        img: "../assets/icon0.jpg",
+        img: "assets/icon0.jpg",
     },
 ]
 
